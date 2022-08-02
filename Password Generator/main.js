9a7fecd4aa43f9c96b1a8da60c6201602447bad1
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2022-08-02 16:41:17 +0200

    Add files via upload

---
+++ Password Generator/main.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let password = "";
+
+let exampleOne = document.getElementById("example-1");
+let exampleTwo = document.getElementById("example-2");
+
+function generatePassword() {
+  cleanUp();
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomSimb = Math.floor(Math.random() * characters.length);
+    exampleOne.textContent += characters[randomSimb];
+  }
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomSimb = Math.floor(Math.random() * characters.length);
+    exampleTwo.textContent += characters[randomSimb];
+  }
+}
+
+function cleanUp() {
+  document.getElementById("example-1").innerHTML = "";
+  document.getElementById("example-2").innerHTML = "";
+}
